fa4e319f0376eb200a9c9e6a82485c7f5e23bf69
vikkn <vikaslagerweij@gmail.com>
2021-09-16 19:16:19 +0200

    banaba

--- src/main/java/me/alpha432/oyvey/modules/client/ClickGui.java
+++ src/main/java/me/alpha432/oyvey/modules/client/ClickGui.java
@@ -14,8 +14,6 @@ public class ClickGui
         extends Module {
     private static ClickGui INSTANCE = new ClickGui();
     public Setting<String> prefix = this.register(new Setting<String>("Prefix", "."));
-    public Setting<Boolean> customFov = this.register(new Setting<Boolean>("CustomFov", false));
-    public Setting<Float> fov = this.register(new Setting<Float>("Fov", Float.valueOf(150.0f), Float.valueOf(-180.0f), Float.valueOf(180.0f)));
     public Setting<Integer> red = this.register(new Setting<Integer>("Red", 0, 0, 255));
     public Setting<Integer> green = this.register(new Setting<Integer>("Green", 0, 0, 255));
     public Setting<Integer> blue = this.register(new Setting<Integer>("Blue", 255, 0, 255));
@@ -25,9 +23,9 @@ public class ClickGui
     public Setting<Integer> topBlue = this.register(new Setting<Integer>("SecondBlue", 150, 0, 255));
     public Setting<Integer> alpha = this.register(new Setting<Integer>("HoverAlpha", 240, 0, 255));
     public Setting<Boolean> rainbow = this.register(new Setting<Boolean>("Rainbow", false));
-    public Setting<rainbowMode> rainbowModeHud = this.register(new Setting<Object>("HRainbowMode", rainbowMode.Static, v -> this.rainbow.getValue()));
-    public Setting<rainbowModeArray> rainbowModeA = this.register(new Setting<Object>("ARainbowMode", rainbowModeArray.Static, v -> this.rainbow.getValue()));
-    public Setting<Integer> rainbowHue = this.register(new Setting<Object>("Delay", Integer.valueOf(240), Integer.valueOf(0), Integer.valueOf(600), v -> this.rainbow.getValue()));
+    public Setting<rainbowMode> rainbowModeHud = register(new Setting<Object>("HRainbowMode", rainbowMode.Static, v -> this.rainbow.getValue()));
+    public Setting<rainbowModeArray> rainbowModeA = register(new Setting<Object>("ARainbowMode", rainbowModeArray.Static, v -> this.rainbow.getValue()));
+    public Setting<Integer> rainbowHue = register(new Setting<Object>("Delay", Integer.valueOf(240), Integer.valueOf(0), Integer.valueOf(600), v -> this.rainbow.getValue()));
     public Setting<Float> rainbowBrightness = this.register(new Setting<Object>("Brightness ", Float.valueOf(150.0f), Float.valueOf(1.0f), Float.valueOf(255.0f), v -> this.rainbow.getValue()));
     public Setting<Float> rainbowSaturation = this.register(new Setting<Object>("Saturation", Float.valueOf(150.0f), Float.valueOf(1.0f), Float.valueOf(255.0f), v -> this.rainbow.getValue()));
     private OyVeyGui click;
@@ -46,13 +44,6 @@ public class ClickGui
 
     private void setInstance() {
         INSTANCE = this;
-    }
-
-    @Override
-    public void onUpdate() {
-        if (this.customFov.getValue()) {
-            OyVey.INSTANCE.mc.gameSettings.setOptionFloatValue(GameSettings.Options.FOV, this.fov.getValue().floatValue());
-        }
     }
 
     @SubscribeEvent
