517b9da7c6c7112eb7babd9c8eab76d8d62ffdf6
jewbob <69072843+jewbob@users.noreply.github.com>
2021-09-17 21:54:38 +0100

    why would you remove this @zpres!!!

---
+++ src/main/java/mint/utils/Global.java
@@ -0,0 +1,10 @@
+package mint.utils;
+
+import net.minecraft.client.Minecraft;
+/**
+ * @author FB
+ * @since 13th January 2021
+ */
+public interface Global {
+    Minecraft mc = Minecraft.getMinecraft();
+}
