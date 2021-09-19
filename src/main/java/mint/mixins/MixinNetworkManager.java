59acfef84f0f775ac01c72bc609fa4d691e4fbe5
Zihasz <40296732+zihasz@users.noreply.github.com>
2021-09-19 19:16:24 +0200

    Fix NetworkManager mixin
    Read my notes

--- src/main/java/mint/mixins/MixinNetworkManager.java
+++ src/main/java/mint/mixins/MixinNetworkManager.java
@@ -10,10 +10,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = {NetworkManager.class})
-public class MixinNetworkManager {
-    @Inject(method = {"sendPacket(Lnet/minecraft/network/Packet;)V"}, at = {@At(value = "HEAD")}, cancellable = true)
-    private void onSendPacketPre(Packet<?> packet, CallbackInfo info) {
+@Mixin(value = NetworkManager.class)
+public abstract class MixinNetworkManager {
+
+    /*
+        1. You dont need the {} for @Inject parameters they are ugly asf
+        2. All mixins should be abstract for convinience
+        3. ALL METHODS MUST BE PUBLIC
+    */
+    
+    @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;)V", at = @At(value = "HEAD"), cancellable = true)
+    public void onSendPacketPre(Packet<?> packet, CallbackInfo info) {
         PacketEvent.Send event = new PacketEvent.Send(0, packet);
         MinecraftForge.EVENT_BUS.post(event);
         if (event.isCanceled()) {
@@ -21,13 +28,13 @@ public class MixinNetworkManager {
         }
     }
 
-    @Inject(method = {"channelRead0"}, at = {@At(value = "HEAD")}, cancellable = true)
-    private void onChannelReadPre(ChannelHandlerContext context, Packet<?> packet, CallbackInfo info) {
+    @Inject(method = "channelRead0", at = @At(value = "HEAD"), cancellable = true)
+    public void onChannelReadPre(ChannelHandlerContext context, Packet<?> packet, CallbackInfo info) {
         PacketEvent.Receive event = new PacketEvent.Receive(0, packet);
         MinecraftForge.EVENT_BUS.post(event);
         if (event.isCanceled()) {
             info.cancel();
         }
     }
+    
 }
-
