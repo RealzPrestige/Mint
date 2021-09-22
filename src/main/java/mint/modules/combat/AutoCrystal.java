6692fcbe49a38de4e29bd4f37cbd695ac879827d
vikkn <vikaslagerweij@gmail.com>
2021-09-22 23:52:04 +0200

    ez ca

--- src/main/java/mint/modules/combat/AutoCrystal.java
+++ src/main/java/mint/modules/combat/AutoCrystal.java
@@ -1,22 +1,25 @@
 package mint.modules.combat;
 
 import mint.clickgui.setting.Setting;
-import mint.commands.Command;
+import mint.events.PacketEvent;
+import mint.events.Render3DEvent;
 import mint.modules.Module;
-import mint.utils.BlockUtil;
-import mint.utils.EntityUtil;
-import mint.utils.MathUtil;
+import mint.utils.*;
+import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityEnderCrystal;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
 import net.minecraft.network.play.client.CPacketUseEntity;
+import net.minecraft.network.play.server.SPacketSoundEffect;
+import net.minecraft.network.play.server.SPacketSpawnObject;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
-
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import java.awt.*;
 import java.util.List;
 
 public class AutoCrystal extends Module {
@@ -28,69 +31,102 @@ public class AutoCrystal extends Module {
 
     public Setting<Float> minDamage = register(new Setting("Min Damage", 6f, 0f, 12f));
     public Setting<Float> maxSelfDamage = register(new Setting("Max Self Damage", 8f, 0f, 12f));
-    public Setting<Float> minHealth = register(new Setting("Min Health", 10f, 0f, 36f));
+
+    public Setting<Integer> placeDelay = register(new Setting("Place Delay", 70, 0, 200));
+
+    Timer placeTimer = new Timer();
+    BlockPos finalPos;
+    BlockPos finalCrystalPos;
+    int crystals;
 
     public AutoCrystal(){
         super("AutoCrystal", Category.COMBAT, "");
     }
 
+    public void onToggle(){
+        mc.world.removeEntityFromWorld(crystals);
+        crystals = 0;
+    }
     @Override
-    public void onUpdate(){
-        if(EntityUtil.getTarget(targetRange.getValue()) == null){
-            return;
+    public void onUpdate() {
+        if (EntityUtil.getTarget(targetRange.getValue()) != null) {
+            if(placeTimer.passedMs(placeDelay.getValue())) {
+                doPlace();
+                placeTimer.reset();
+            }
         }
-        doPlace(EntityUtil.getTarget(targetRange.getValue()), minDamage.getValue(), maxSelfDamage.getValue(), placeRange.getValue(), minHealth.getValue());
-        doBreak(EntityUtil.getTarget(targetRange.getValue()), minDamage.getValue(), maxSelfDamage.getValue(), breakRange.getValue(), minHealth.getValue());
     }
 
-    public void doPlace(EntityPlayer target, float minDamage, float maxSelfDamage, float placeRange, float minHealth) {
+    private void doPlace() {
         BlockPos placePos = null;
-        if (target == null) {
-            return;
-        }
-        final List<BlockPos> sphere = BlockUtil.getSphere(placeRange, true);
+        float maxDamage = 0.5f;
+        final List<BlockPos> sphere = BlockUtil.getSphere(this.placeRange.getValue(), true);
         for (int size = sphere.size(), i = 0; i < size; ++i) {
-            BlockPos pos = sphere.get(i);
-            float targetDamage = EntityUtil.calculatePos(pos, target);
-            float selfDamage = EntityUtil.calculatePos(pos, mc.player);
+            final BlockPos pos = sphere.get(i);
+            final float self = EntityUtil.calculatePos(pos, mc.player);
             if (BlockUtil.canPlaceCrystal(pos, true)) {
-                if(selfDamage > maxSelfDamage){
-                    return;
-                }
-                if(targetDamage > minDamage){
-                    return;
-                }
-                if(selfDamage < minHealth){
-                    return;
-                }
+                final float damage;
+                if (EntityUtil.getHealth(mc.player) > self + 0.5f && this.maxSelfDamage.getValue() > self && (damage = EntityUtil.calculatePos(pos, EntityUtil.getTarget(targetRange.getValue()))) > maxDamage && damage > self) {
+                    if (damage <= this.minDamage.getValue()) {
+                        if (damage <= 2.0f) {
+                            continue;
+                        }
+                    }
+                    maxDamage = damage;
                     placePos = pos;
+                    finalPos = placePos;
+                }
             }
+
         }
-        if(placePos != null){
+        if (placePos != null) {
             mc.getConnection().sendPacket(new CPacketPlayerTryUseItemOnBlock(placePos, EnumFacing.UP, mc.player.getHeldItemOffhand().getItem()== Items.END_CRYSTAL ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND, 0.5f, 0.5f, 0.5f));
+
+            Timer timers = new Timer();
+                EntityEnderCrystal crystal = new EntityEnderCrystal(mc.world, (double) placePos.getX() + 0.5, (double) placePos.getY() + 1, (double) placePos.getZ() + 0.5);
+                mc.world.addEntityToWorld(crystals, crystal);
+                timers.reset();
+                if (timers.passedMs(10)) {
+                    mc.world.removeEntityFromWorld(crystals);
+                    ++crystals;
+            }
         }
     }
 
-    public void doBreak(EntityPlayer target, float minDamage, float maxSelfDamage, float breakRange, float minHealth) {
-        for (Entity crystal : mc.world.loadedEntityList) {
-            if (crystal instanceof EntityEnderCrystal) {
-                BlockPos crystalPos = crystal.getPosition();
-                float targetDamage = EntityUtil.calculatePos(crystalPos, target);
-                float selfDamage = EntityUtil.calculatePos(crystalPos, mc.player);
-                if (selfDamage > maxSelfDamage) {
-                    return;
+    @SubscribeEvent
+    public void onPacketReceive(PacketEvent.Receive event){
+            if (event.getPacket() instanceof SPacketSoundEffect) {
+                final SPacketSoundEffect packet = event.getPacket();
+                if (packet.getCategory() == SoundCategory.BLOCKS && packet.getSound() == SoundEvents.ENTITY_GENERIC_EXPLODE) {
+                    for (Entity entityCrystal : mc.world.loadedEntityList) {
+                        if (entityCrystal instanceof EntityEnderCrystal) {
+                            if (entityCrystal.getDistance(packet.getX(), packet.getY(), packet.getZ()) <= breakRange.getValue()) {
+                                    entityCrystal.setDead();
+                        }
+                    }
                 }
-                if (targetDamage < minDamage) {
-                    return;
-                }
-                if (selfDamage > minHealth) {
-                    return;
-                }
-                if (crystal.getDistance(mc.player) > MathUtil.square(breakRange)) {
-                    return;
-                }
-                mc.getConnection().sendPacket(new CPacketUseEntity(crystal));
             }
         }
+        if (event.getPacket() instanceof SPacketSpawnObject) {
+            final SPacketSpawnObject packet = event.getPacket();
+            if (packet.getType() == 51 && finalPos != null && EntityUtil.getTarget(targetRange.getValue()) != null) {
+                final CPacketUseEntity predict = new CPacketUseEntity();
+                predict.entityId = packet.getEntityID();
+                predict.action = CPacketUseEntity.Action.ATTACK;
+                if(predict.entityId != crystals) {
+                    mc.getConnection().sendPacket(predict);
+                }
+            }
+        }
+    }
+
+    public void onRender3D(Render3DEvent event){
+        if(finalPos != null){
+            RenderUtil.drawBox(finalPos, new Color(0, 255, 0, 100));
+        }
+        if(finalCrystalPos != null){
+            RenderUtil.drawBox(finalCrystalPos, new Color(255, 0, 0, 100));
+        }
+
     }
 }
