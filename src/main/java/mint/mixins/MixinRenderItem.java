d39f250f8469b0078784532ea5da11b8ea91cd5d
vikkn <vikaslagerweij@gmail.com>
2021-10-13 20:48:13 +0200

    Item Opacity

--- src/main/java/mint/mixins/MixinRenderItem.java
+++ src/main/java/mint/mixins/MixinRenderItem.java
@@ -1,6 +1,7 @@
 package mint.mixins;
 
 import mint.events.RenderItemEvent;
+import mint.modules.visual.Hand;
 import mint.modules.visual.ViewTweaks;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderItem;
@@ -19,10 +20,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.awt.*;
 
 /**
- *
  * @author zPrestige_
  * 13/10/2021
- *
  * @author FB for itemOpacityChanger (inspired by Phobos.EU / Abyss)
  */
 
@@ -32,6 +31,7 @@ public abstract class MixinRenderItem {
     @Shadow
     private void renderModel(IBakedModel model, int color, ItemStack stack) {
     }
+
     @Inject(method = {"renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/client/renderer/block/model/ItemCameraTransforms$TransformType;Z)V"}, at = {@At("INVOKE")})
     public void renderItem(final ItemStack stack, final EntityLivingBase entityLivingBaseIn, final ItemCameraTransforms.TransformType transform, final boolean leftHanded, final CallbackInfo ci) {
         if (transform == ItemCameraTransforms.TransformType.FIRST_PERSON_LEFT_HAND || transform == ItemCameraTransforms.TransformType.FIRST_PERSON_RIGHT_HAND) {
@@ -41,6 +41,7 @@ public abstract class MixinRenderItem {
                 MinecraftForge.EVENT_BUS.post(new RenderItemEvent.MainHand(stack));
         }
     }
+
     @Redirect(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/renderer/block/model/IBakedModel;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;color(FFFF)V"))
     private void itemColour(float colorRed, float colorGreen, float colorBlue, float colorAlpha) {
         if (ViewTweaks.getInstance() != null && ViewTweaks.getInstance().isEnabled()) {
@@ -52,10 +53,10 @@ public abstract class MixinRenderItem {
 
     @Redirect(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/renderer/block/model/IBakedModel;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderItem;renderModel(Lnet/minecraft/client/renderer/block/model/IBakedModel;Lnet/minecraft/item/ItemStack;)V"))
     private void renderModelColor(RenderItem renderItem, IBakedModel model, ItemStack stack) {
-        if (ViewTweaks.getInstance() != null && ViewTweaks.getInstance().isEnabled()) {
-            renderModel(model, new Color(1f, 1f, 1f, ViewTweaks.getInstance().alpha.getValue()).getRGB(), stack);
-        } else {
-            renderModel(model, -1, stack);
-        }
+        if (!Hand.getInstance().isEnabled())
+            return;
+
+        renderModel(model, new Color(1f, 1f, 1f, Hand.getInstance().alpha.getValue() / 255.0f).getRGB(), stack);
+
     }
 }
--- src/main/java/mint/modules/visual/Hand.java
+++ src/main/java/mint/modules/visual/Hand.java
@@ -8,42 +8,59 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.opengl.GL11;
 
 public class Hand extends Module {
-    public Setting<Boolean> mainhandParent = register(new Setting("Mainhand", true, false));
+    static Hand INSTANCE = new Hand();
 
-    public Setting<Boolean> mainhandTranslation = register(new Setting("Mainhand Translation", false, true, v -> mainhandParent.getValue()));
-    public Setting<Float> mainhandX = register(new Setting("Mainhand X", 0.0f, -10.0f, 10.0f, v -> mainhandParent.getValue() && mainhandTranslation.getValue()));
-    public Setting<Float> mainhandY = register(new Setting("Mainhand Y", 0.0f, -10.0f, 10.0f, v -> mainhandParent.getValue() && mainhandTranslation.getValue()));
-    public Setting<Float> mainhandZ = register(new Setting("Mainhand Z", 0.0f, -10.0f, 10.0f, v -> mainhandParent.getValue() && mainhandTranslation.getValue()));
+    public Setting<Boolean> mainhandParent = register(new Setting<>("Mainhand", true, false));
 
-    public Setting<Boolean> mainhandScaling = register(new Setting("Mainhand Scaling",false, true, v -> mainhandParent.getValue()));
-    public Setting<Float> mainhandScaleX = register(new Setting("Mainhand Scale X", 0.0f, -10.0f, 10.0f, v -> mainhandParent.getValue() && mainhandScaling.getValue()));
-    public Setting<Float> mainhandScaleY = register(new Setting("Mainhand Scale Y", 0.0f, -10.0f, 10.0f, v -> mainhandParent.getValue() && mainhandScaling.getValue()));
-    public Setting<Float> mainhandScaleZ = register(new Setting("Mainhand Scale Z", 0.0f, -10.0f, 10.0f, v -> mainhandParent.getValue() && mainhandScaling.getValue()));
+    public Setting<Boolean> mainhandTranslation = register(new Setting<>("Mainhand Translation", false, true, v -> mainhandParent.getValue()));
+    public Setting<Float> mainhandX = register(new Setting<>("Mainhand X", 0.0f, -10.0f, 10.0f, v -> mainhandParent.getValue() && mainhandTranslation.getValue()));
+    public Setting<Float> mainhandY = register(new Setting<>("Mainhand Y", 0.0f, -10.0f, 10.0f, v -> mainhandParent.getValue() && mainhandTranslation.getValue()));
+    public Setting<Float> mainhandZ = register(new Setting<>("Mainhand Z", 0.0f, -10.0f, 10.0f, v -> mainhandParent.getValue() && mainhandTranslation.getValue()));
 
-    public Setting<Boolean> mainhandRotation = register(new Setting("Mainhand Rotation", false, true, v -> mainhandParent.getValue()));
-    public Setting<Float> mainhandRotationX = register(new Setting("Mainhand Rotation X", 0.0f, 0.0f, 10.0f, v -> mainhandParent.getValue() && mainhandRotation.getValue()));
-    public Setting<Float> mainhandRotationY = register(new Setting("Mainhand Rotation Y", 0.0f, 0.0f, 10.0f, v -> mainhandParent.getValue() && mainhandRotation.getValue()));
-    public Setting<Float> mainhandRotationZ = register(new Setting("Mainhand Rotation Z", 0.0f, 0.0f, 10.0f, v -> mainhandParent.getValue() && mainhandRotation.getValue()));
+    public Setting<Boolean> mainhandScaling = register(new Setting<>("Mainhand Scaling", false, true, v -> mainhandParent.getValue()));
+    public Setting<Float> mainhandScaleX = register(new Setting<>("Mainhand Scale X", 0.0f, -10.0f, 10.0f, v -> mainhandParent.getValue() && mainhandScaling.getValue()));
+    public Setting<Float> mainhandScaleY = register(new Setting<>("Mainhand Scale Y", 0.0f, -10.0f, 10.0f, v -> mainhandParent.getValue() && mainhandScaling.getValue()));
+    public Setting<Float> mainhandScaleZ = register(new Setting<>("Mainhand Scale Z", 0.0f, -10.0f, 10.0f, v -> mainhandParent.getValue() && mainhandScaling.getValue()));
 
-    public Setting<Boolean> offhandParent = register(new Setting("Offhand", true, false));
+    public Setting<Boolean> mainhandRotation = register(new Setting<>("Mainhand Rotation", false, true, v -> mainhandParent.getValue()));
+    public Setting<Float> mainhandRotationX = register(new Setting<>("Mainhand Rotation X", 0.0f, 0.0f, 10.0f, v -> mainhandParent.getValue() && mainhandRotation.getValue()));
+    public Setting<Float> mainhandRotationY = register(new Setting<>("Mainhand Rotation Y", 0.0f, 0.0f, 10.0f, v -> mainhandParent.getValue() && mainhandRotation.getValue()));
+    public Setting<Float> mainhandRotationZ = register(new Setting<>("Mainhand Rotation Z", 0.0f, 0.0f, 10.0f, v -> mainhandParent.getValue() && mainhandRotation.getValue()));
 
-    public Setting<Boolean> offhandTranslation = register(new Setting("Offhand Translation", false, true, v -> offhandParent.getValue()));
-    public Setting<Float> offhandX = register(new Setting("Offhand X", 0.0f, -10.0f, 10.0f, v -> offhandParent.getValue() && offhandTranslation.getValue()));
-    public Setting<Float> offhandY = register(new Setting("Offhand Y", 0.0f, -10.0f, 10.0f, v -> offhandParent.getValue() && offhandTranslation.getValue()));
-    public Setting<Float> offhandZ = register(new Setting("Offhand Z", 0.0f, -10.0f, 10.0f, v -> offhandParent.getValue() && offhandTranslation.getValue()));
+    public Setting<Boolean> offhandParent = register(new Setting<>("Offhand", true, false));
 
-    public Setting<Boolean> offhandScaling = register(new Setting("Offhand Scaling", false, true, v -> offhandParent.getValue()));
-    public Setting<Float> offhandScaleX = register(new Setting("Offhand Scale X", 0.0f, -10.0f, 10.0f, v -> offhandParent.getValue() && offhandScaling.getValue()));
-    public Setting<Float> offhandScaleY = register(new Setting("Offhand Scale Y", 0.0f, -10.0f, 10.0f, v -> offhandParent.getValue() && offhandScaling.getValue()));
-    public Setting<Float> offhandScaleZ = register(new Setting("Offhand Scale Z", 0.0f, -10.0f, 10.0f, v -> offhandParent.getValue() && offhandScaling.getValue()));
+    public Setting<Boolean> offhandTranslation = register(new Setting<>("Offhand Translation", false, true, v -> offhandParent.getValue()));
+    public Setting<Float> offhandX = register(new Setting<>("Offhand X", 0.0f, -10.0f, 10.0f, v -> offhandParent.getValue() && offhandTranslation.getValue()));
+    public Setting<Float> offhandY = register(new Setting<>("Offhand Y", 0.0f, -10.0f, 10.0f, v -> offhandParent.getValue() && offhandTranslation.getValue()));
+    public Setting<Float> offhandZ = register(new Setting<>("Offhand Z", 0.0f, -10.0f, 10.0f, v -> offhandParent.getValue() && offhandTranslation.getValue()));
 
-    public Setting<Boolean> offhandRotation = register(new Setting("Offhand Rotation", false, true, v -> offhandParent.getValue()));
-    public Setting<Float> offhandRotationX = register(new Setting("Offhand Rotation X", 0.0f, 0.0f, 10.0f, v -> offhandParent.getValue() && offhandRotation.getValue()));
-    public Setting<Float> offhandRotationY = register(new Setting("Offhand Rotation Y", 0.0f, 0.0f, 10.0f, v -> offhandParent.getValue() && offhandRotation.getValue()));
-    public Setting<Float> offhandRotationZ = register(new Setting("Offhand Rotation Z", 0.0f, 0.0f, 10.0f, v -> offhandParent.getValue() && offhandRotation.getValue()));
+    public Setting<Boolean> offhandScaling = register(new Setting<>("Offhand Scaling", false, true, v -> offhandParent.getValue()));
+    public Setting<Float> offhandScaleX = register(new Setting<>("Offhand Scale X", 0.0f, -10.0f, 10.0f, v -> offhandParent.getValue() && offhandScaling.getValue()));
+    public Setting<Float> offhandScaleY = register(new Setting<>("Offhand Scale Y", 0.0f, -10.0f, 10.0f, v -> offhandParent.getValue() && offhandScaling.getValue()));
+    public Setting<Float> offhandScaleZ = register(new Setting<>("Offhand Scale Z", 0.0f, -10.0f, 10.0f, v -> offhandParent.getValue() && offhandScaling.getValue()));
+
+    public Setting<Boolean> offhandRotation = register(new Setting<>("Offhand Rotation", false, true, v -> offhandParent.getValue()));
+    public Setting<Float> offhandRotationX = register(new Setting<>("Offhand Rotation X", 0.0f, 0.0f, 10.0f, v -> offhandParent.getValue() && offhandRotation.getValue()));
+    public Setting<Float> offhandRotationY = register(new Setting<>("Offhand Rotation Y", 0.0f, 0.0f, 10.0f, v -> offhandParent.getValue() && offhandRotation.getValue()));
+    public Setting<Float> offhandRotationZ = register(new Setting<>("Offhand Rotation Z", 0.0f, 0.0f, 10.0f, v -> offhandParent.getValue() && offhandRotation.getValue()));
+
+    public Setting<Float> alpha = register(new Setting("Item Opacity", 255.0f, 0.0f, 255.0f));
+
 
     public Hand() {
         super("Hand", Category.VISUAL, "");
+        this.setInstance();
+    }
+
+    public static Hand getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Hand();
+        }
+        return INSTANCE;
+    }
+
+    private void setInstance() {
+        INSTANCE = this;
     }
 
     @SubscribeEvent
